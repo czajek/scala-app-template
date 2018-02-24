f40b921a230a4019b88ac35f5a43960ca529701a
Wszolek Jakub - jwszol <jwszolek@eti.pg.gda.pl>
2018-02-24 02:05:59 +0100

    initial commit
    initial commit

---
+++ src/main/scala/com/datamass/Driver.scala
@@ -0,0 +1,12 @@
+package com.datamass
+
+import org.apache.log4j.Logger
+
+object Driver {
+  private val logger = Logger.getLogger(getClass)
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
