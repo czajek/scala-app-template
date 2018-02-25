2211de25ef3f4f1cb315be4898e3e5b94a7b8363
Wszolek Jakub - jwszol <jwszolek@eti.pg.gda.pl>
2018-02-25 20:38:46 +0100

    testing logging added

--- src/main/scala/com/datamass/Driver.scala
+++ src/main/scala/com/datamass/Driver.scala
@@ -7,6 +7,13 @@ object Driver {
 
   def main(args: Array[String]): Unit = {
 
+    while(true) {
+      logger.info("Kafka message comes in")
+      Thread.sleep(10000)
+      logger.error("Kafka message error in processing")
+      Thread.sleep(10000)
+    }
+
   }
 
 }
