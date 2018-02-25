27a9107d7032ece0817db72e40b8ba85caa053ce
Wszolek Jakub - jwszol <jwszolek@eti.pg.gda.pl>
2018-02-25 22:28:35 +0100

    removed unused part

--- src/main/scala/com/datamass/Driver.scala
+++ src/main/scala/com/datamass/Driver.scala
@@ -6,10 +6,7 @@ object Driver {
   private val logger = Logger.getLogger(getClass)
 
   def main(args: Array[String]): Unit = {
-
-    import org.apache.log4j.PropertyConfigurator
-    PropertyConfigurator.configure("log4j.properties")
-
+    
     while(true) {
       logger.info("Kafka message comes in")
       Thread.sleep(10000)
